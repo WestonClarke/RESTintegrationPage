27d9ddd5e53cfbd06dde14c423a1734fbab9298a
WestonClarke <weston.d.clarke@gmail.com>
2014-06-16 15:10:07 +0000

    typo fixes

--- submit.php
+++ submit.php
@@ -1,6 +1,6 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
     
     if (array_key_exists("card", $_POST) && array_key_exists("fname", $_POST) && array_key_exists("lname", $_POST) && array_key_exists("username", $_POST) && array_key_exists("pw", $_POST)){
         
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
                 if ($_POST['pw'] != ""){
                     
                     //Required fields are validated here 
-                    echo "Success";
+                    echo json_encode(array("printedCardNumber"=>$_POST['card'], "username"=>$_POST['username'], "password"=>$_POST['pw'] ));
                     
                 } else {
                     
@@ -32,6 +32,10 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
         }
         
     }
+    
+} else {
+    
+    echo "Request method not working";
     
 }
 
